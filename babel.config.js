708eb92a2f19f9e2d4b7aa51c01872357eeb2e4f
o-Ha-minor <tuba1042855@yahoo.co.jp>
2025-01-12 01:10:39 +0900

    fix lock files and migrate

--- babel.config.js
+++ babel.config.js
@@ -14,7 +14,7 @@ module.exports = function(api) {
         '.'
     )
   }
-  
+
   return {
     presets: [
       isTestEnv && [
--- db/migrate/20250110123643_drop_coments_table.rb
+++
@@ -1,5 +0,0 @@
-class DropCommentsTable < ActiveRecord::Migration[8.0]
-    def change
-      drop_table :comments, if_exists: true
-    end
-end
