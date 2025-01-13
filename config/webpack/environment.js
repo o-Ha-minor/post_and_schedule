53b489e9a2e9ec273ea0cb73addcca9657fa90eb
o-Ha-minor <tuba1042855@yahoo.co.jp>
2025-01-13 17:36:31 +0900

    Revert "Fix Webpack configuration"
    This reverts commit c60da11858c4e89c0c489ccd8a3d128110bb7704.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,3 @@
 const { environment } = require('@rails/webpacker')
-environment.config.merge({
-    node: {
-      global: true,
-      __dirname: false,
-      __filename: false,
-    }
-  })
-  
+
 module.exports = environment
