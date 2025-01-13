bf7af20bc19fba6bdd99b000023b789b14bb41bf
o-Ha-minor <tuba1042855@yahoo.co.jp>
2025-01-13 11:21:46 +0900

    Fix Webpack configuration

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Node.jsビルドを先に実行
-yarn install
-yarn build
-
-# Railsのビルドを実行
-bundle install
-bundle exec rake assets:precompile
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
-
+environment.config.merge({
+    node: {
+      global: true,
+      __dirname: false,
+      __filename: false,
+    }
+  })
+  
 module.exports = environment
