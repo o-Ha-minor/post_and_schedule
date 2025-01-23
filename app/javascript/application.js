43596771599863ee01518854e1bb68b27c82eeb3
o-Ha-minor <tuba1042855@yahoo.co.jp>
2025-01-24 00:25:48 +0900

    add javascript's file

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-document.addEventListener('DOMContentLoaded', function () {
-    const menuButton = document.getElementById('menu-button');
-    const menu = document.getElementById('menu');
-  
-    menuButton.addEventListener('click', function () {
-      // メニューの表示/非表示を切り替え
-      if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-      } else {
-        menu.style.display = 'block';
-      }
-    });
-  });
-  
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "controllers"
+import "menu"
+
