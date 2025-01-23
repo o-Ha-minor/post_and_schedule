fcfefb47f4e0de2da998652b88c8c7a5d04067d9
o-Ha-minor <tuba1042855@yahoo.co.jp>
2025-01-23 22:54:32 +0900

    alter menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+document.addEventListener('DOMContentLoaded', function () {
+    const menuButton = document.getElementById('menu-button');
+    const menu = document.getElementById('menu');
+  
+    menuButton.addEventListener('click', function () {
+      // メニューの表示/非表示を切り替え
+      if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+      } else {
+        menu.style.display = 'block';
+      }
+    });
+  });
+  
