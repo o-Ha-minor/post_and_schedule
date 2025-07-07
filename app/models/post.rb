448aa12d6412f2d2bfecd29d7ae682449d8a88a7
o-Ha-minor <tuba1042855@yahoo.co.jp>
2025-07-08 00:01:37 +0900

    ルート等の整理

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-    has_one_attached :avatar
+    has_one_attached :image
     belongs_to :user
     validates :user_id, presence: true
     has_many :likes, dependent: :destroy
